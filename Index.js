5871b604483f938dbc8b61a2adca25402b531600
Sahil Alam <sahil.nabagram@gmail.com>
2026-04-30 16:48:30 +0530

    Initials Creator done

--- Index.js
+++ Index.js
@@ -137,3 +137,11 @@ function checkType(input) {
 
 checkType(10);
 checkType("hello");
+
+//Initial Creator
+
+let fullName = "John Doe";
+
+let initials = fullName.split(" ")[0][0] + fullName.split(" ")[1][0];
+
+console.log(initials);
