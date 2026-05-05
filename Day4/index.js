8701e9a60f758c9b18547b75fe129bc563945f5f
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-05 12:26:16 +0530

    Synopsis and API fetch code updated

--- Day4/index.js
+++ Day4/index.js
@@ -18,3 +18,34 @@ getData().then((data) => {
 // setInterval(() => {
 //     console.log("Checking for new data...");
 // }, 3000);
+
+// =======================
+// Promise Example
+// =======================
+function getData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Data received");
+    }, 2000);
+  });
+}
+
+getData()
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
+
+
+// =======================
+// Fetch API + Data Processing
+// =======================
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(res => res.json())
+  .then(data => data.filter(d => d.id % 2 === 0)) // keep even IDs
+  .then(data =>
+    data.map(d => ({
+      ...d,
+      title: "Aaryan " + d.title
+    }))
+  )
+  .then(data => console.log(data))
+  .catch(error => console.error("Error fetching data:", error));
