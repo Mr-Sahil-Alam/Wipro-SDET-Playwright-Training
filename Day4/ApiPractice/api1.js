1575c952766bd2e0d6f301e3cc792e711f74b554
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-05 16:09:45 +0530

    All updated

--- Day4/ApiPractice/api1.js
+++ Day4/ApiPractice/api1.js
@@ -1,4 +1,4 @@
-fetch("https://open.er-api.com/v6/latest/USD")
+/*fetch("https://open.er-api.com/v6/latest/USD")
   .then(res => res.json())
   .then(data => {
     console.log("USD to INR:", data.rates.INR);
@@ -6,6 +6,7 @@ fetch("https://open.er-api.com/v6/latest/USD")
     console.log("USD to GBP:", data.rates.GBP);
   })
   .catch(err => console.error(err));
+  */
 
 //Print value Greater than 90
 fetch("https://open.er-api.com/v6/latest/USD")
@@ -17,3 +18,95 @@ fetch("https://open.er-api.com/v6/latest/USD")
     console.log(result);
   })
   .catch(err => console.error(err));
+
+  // 1. TODOS API
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(r => r.json())
+  .then(a => a.filter(t => t.id <= 10))
+  .then(a => a.map(t => t.title))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 2. POSTS API
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(r => r.json())
+  .then(a => a.filter(p => p.userId === 1))
+  .then(a => a.map(p => p.title))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 3. COMMENTS API
+fetch("https://jsonplaceholder.typicode.com/comments")
+  .then(r => r.json())
+  .then(a => a.filter(c => c.id <= 10))
+  .then(a => a.map(c => c.email))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 4. USERS API
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(r => r.json())
+  .then(a => a.filter(u => u.id <= 5))
+  .then(a => a.map(u => u.name))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 5. ALBUMS API
+fetch("https://jsonplaceholder.typicode.com/albums")
+  .then(r => r.json())
+  .then(a => a.filter(al => al.id <= 10))
+  .then(a => a.map(al => al.title))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 6. PHOTOS API
+fetch("https://jsonplaceholder.typicode.com/photos")
+  .then(r => r.json())
+  .then(a => a.filter(p => p.id <= 5))
+  .then(a => a.map(p => p.url))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 7. CAT FACTS API
+fetch("https://catfact.ninja/facts")
+  .then(r => r.json())
+  .then(obj => obj.data)
+  .then(a => a.filter(f => f.length < 100))
+  .then(a => a.map(f => f.fact))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 8. DOG FACTS API
+fetch("https://dogapi.dog/api/v2/facts")
+  .then(r => r.json())
+  .then(obj => obj.data)
+  .then(a => a.map(d => d.attributes.body))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 9. RANDOM USER API
+fetch("https://randomuser.me/api/?results=10")
+  .then(r => r.json())
+  .then(obj => obj.results)
+  .then(a => a.filter(u => u.gender === "female"))
+  .then(a => a.map(u => u.name.first))
+  .then(console.log)
+  .catch(console.error);
+
+
+// 10. POKEMON API
+fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  .then(r => r.json())
+  .then(obj => obj.results)
+  .then(a => a.filter(p => p.name.startsWith("b")))
+  .then(a => a.map(p => p.name))
+  .then(console.log)
+  .catch(console.error);
