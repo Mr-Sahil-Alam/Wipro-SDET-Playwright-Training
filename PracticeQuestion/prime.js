2f74c57a701b28999ad65a0cb3c5cb26e02c2c64
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-09 10:50:24 +0530

    Prime Number

---
+++ PracticeQuestion/prime.js
@@ -0,0 +1,21 @@
+//Check whether a number is prime.
+function PrimeNumber(num) {
+
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i++) {
+
+    if (num % i === 0) {
+
+      return false;
+
+    }
+
+  }
+
+  return true;
+}
+
+console.log(PrimeNumber(7));
