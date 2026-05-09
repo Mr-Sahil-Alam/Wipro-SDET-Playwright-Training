791420b18a5b312c9c1b3ece2611614c8339d546
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-09 11:01:46 +0530

    Factorial

---
+++ PracticeQuestion/factorial.js
@@ -0,0 +1,15 @@
+//Find factorial of a number using loops.
+ function factorial(num) {
+
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+
+    result = result * i;
+
+  }
+
+  return result;
+}
+
+console.log(factorial(5));
