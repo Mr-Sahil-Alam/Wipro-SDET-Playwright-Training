3fdda3b570acbdce1bf28926ee59c938c1fcd009
Sahil Alam <sahil.nabagram@gmail.com>
2026-05-13 11:16:22 +0530

    Question 6 done

---
+++ Day11/Assignment6.ts
@@ -0,0 +1,23 @@
+/*
+Conditional Types & the infer Keyword
+Scenario: You are working with a library that returns data wrapped in a Promise, and you need
+to extract the underlying type.
+Task: Create a utility type UnwrapPromise<T>. It should check if T is a Promise. If it is,
+use the infer keyword to return the type the promise resolves to; otherwise, return T itself.
+
+*/
+type UnwrapPromise<T> =
+
+    T extends Promise<infer U>
+
+    ? U
+
+    : T;
+
+
+
+type A = UnwrapPromise<Promise<string>>;
+
+type B = UnwrapPromise<Promise<number>>;
+
+type C = UnwrapPromise<boolean>;
